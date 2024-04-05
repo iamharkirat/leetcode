869566f0c14d1479b778744b60fc000617cae4f8
iamharkirat <hchahal1@asu.edu>
2024-04-05 12:50:22 -0500

    leetcode

---
+++ binary_search/35. Search Insert Position.py
@@ -0,0 +1,21 @@
+nums = [1,3,5,6]
+target = 7
+
+def searchInsert(nums, target):
+    l, r = 0, len(nums)-1
+
+    while l <= r:
+        mid = (l+r)//2
+
+        if target == nums[mid]:
+            return mid
+
+        if target > nums[mid]:
+            l = mid+1
+        else:
+            r = mid-1
+
+    return l
+
+x = searchInsert(nums=nums, target=target)
+print(x)
